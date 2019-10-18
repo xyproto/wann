21686872f85c3ca914f67ce40f67011cb84c161d
Alexander F. Rødseth <rodseth@gmail.com>
2019-10-18 14:04:54 +0200

    Document the configuration fields

--- config.go
+++ config.go
@@ -4,7 +4,10 @@ package wann
 // The idea is that referring to fields by name is more explicit, and that it can
 // be re-used in connection with having a configuration file, in the future.
 type Config struct {
-	Inputs          int
+	// Number of input neurons
+	Inputs int
+	// When initializing a network, this is the propability that the node will be connected to the output node
 	ConnectionRatio float64
-	SharedWeight    float64
+	// SharedWeight is the weight that is shared by all nodes, since this is a Weight Agnostic Neural Network
+	SharedWeight float64
 }
