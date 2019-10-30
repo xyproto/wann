4b66ccd59903078be64057af7cb0dfbde8bdfa0d
Alexander F. Rødseth <rodseth@gmail.com>
2019-10-30 12:56:37 +0100

    Adjust the simple example

--- cmd/simple/main.go
+++ cmd/simple/main.go
@@ -2,14 +2,10 @@ package main
 
 import (
 	"fmt"
+
 	"github.com/xyproto/wann"
 )
 
 func main() {
-	net := wann.NewNetwork(&wann.Config{
-		Inputs:          5,
-		ConnectionRatio: 0.5,
-		SharedWeight:    0.5,
-	})
-	fmt.Println(net)
+	fmt.Println(wann.NewNetwork())
 }
